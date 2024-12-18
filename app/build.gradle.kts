fac58ee237b83453d575023c52582a45667239bf
danovs <dsludanov@gmail.com>
2024-12-18 14:33:39 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.example.weatherapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.weatherapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.sdpAndroid)
+    implementation(libs.sspAndroid)
+
+    // Navigation
+    implementation(libs.navigationFragment)
+    implementation(libs.navigationUi)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converterGson)
+
+    // Coil
+    implementation(libs.coilCompose)
+
+    // Koin
+    implementation(libs.koinAndroid)
+    implementation(libs.koinAndroidxCompose)
+
+    // ViewModel KTX
+    implementation(libs.viewmodelKtx)
+}
